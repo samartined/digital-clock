744d48ec05458d028cb1c755d19dab139710c515
Edgar <espinosamartinedgar@gmail.com>
2023-12-01 12:41:51 +0100

    Gradient background added, and font and its size has been changed too.

--- src/main/java/com/relojdigital/interfaz/MyFrame.java
+++ src/main/java/com/relojdigital/interfaz/MyFrame.java
@@ -1,12 +1,17 @@
 package com.relojdigital.interfaz;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import com.relojdigital.back.ClockHandler;
 
@@ -15,34 +20,50 @@ public class MyFrame extends JFrame {
     private JLabel timeLabel;
 
     public MyFrame() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Reloj Digital");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Reloj Digital");
 
         // Obtenemos el tamaño de la pantalla
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-        // Calcular el ancho de la ventana pqara que ocupe el 60% de la pantalla
+        // Calcular el ancho de la ventana para que ocupe el 60% de la pantalla
         int windowWidth = (int) (screenSize.width * 0.6);
 
-        this.setSize(windowWidth, windowWidth); // Tamaño de la ventana
-        this.setResizable(false); // No se puede redimensionar
-        this.setLocationRelativeTo(null); // Centrar la ventana
+        setSize(windowWidth, windowWidth); // Tamaño de la ventana
+        setResizable(false); // No se puede redimensionar
+        setLocationRelativeTo(null); // Centrar la ventana
 
+        // Panel para contenido con fondo gradiente
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                GradientPaint gradient = new GradientPaint(0, 0, new Color(30, 30, 30), getWidth(), getHeight(),
+                        new Color(10, 10, 10));
+                g2d.setPaint(gradient);
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+        panel.setLayout(new BorderLayout());
+
+        // Agregar el timeLabel al panel
         timeLabel = new JLabel();
-        timeLabel.setFont(new Font("Arial", Font.PLAIN, 50));
+        timeLabel.setFont(new Font("Arial", Font.BOLD, 100));
+        timeLabel.setHorizontalAlignment(JLabel.CENTER);
         timeLabel.setForeground(new Color(0x00FF00));
-        timeLabel.setBackground(Color.black);
-        timeLabel.setOpaque(true);
 
-        this.add(timeLabel);
+        panel.add(timeLabel, BorderLayout.CENTER);
+
+        add(panel);
+
+        // Hacer visible la ventana después de iniciar el reloj
+        setVisible(true);
     }
 
     public void startClock() {
         // Crear una instancia del manejador del reloj y ejecutarlo en un hilo separado
         ClockHandler clockHandler = new ClockHandler(timeLabel);
         new Thread(clockHandler).start();
-
-        // Hacer visible la ventana después de iniciar el reloj
-        this.setVisible(true);
     }
 }
